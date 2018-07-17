c1c6a14dac629d168976757a43c4f789af50f88c
smat-501 <smat-501@ICT-18>
2018-07-17 14:36:59 +0900

    update

--- spring_portfolio/src/main/java/com/myportfolio/proc/UtilFile.java
+++ spring_portfolio/src/main/java/com/myportfolio/proc/UtilFile.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 import org.springframework.util.FileCopyUtils;
 
 public class UtilFile {
-	String uploadPath="D:\\eclipse_work\\spring_portfolio\\src\\main\\webapp\\file_room";
+	String uploadPath="C:\\Users\\smat-501\\git\\webproject\\spring_portfolio\\src\\main\\webapp\\file_room";
 	
 	public String create(String originalName, byte[] fileData) throws IOException {
 		File target = new File(uploadPath, originalName);
